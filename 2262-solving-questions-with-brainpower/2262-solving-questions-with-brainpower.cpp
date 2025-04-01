02800d7b646ba13711edc27189f81cb258713150
Aditya Sinha <sinhadityaforever@gmail.com>
2025-04-01 22:04:50 +0530

    Time: 0 ms (100%), Space: 119.7 MB (74.65%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -12,7 +12,18 @@ public:
     }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n+1, -1);
-        return solve(questions, 0, n, dp);
+        vector<long long> dp(n+1, 0);
+
+        dp[n] =0;
+
+        for(int i=n-1; i>=0; i--){
+            long long pick =questions[i][0];
+            if(i+questions[i][1]+1<n)
+            pick +=dp[i+questions[i][1]+1];
+            long long notPick = dp[i+1];
+            dp[i] = max(pick, notPick);
+        }
+        // return solve(questions, 0, n, dp);
+        return dp[0];
     }
 };

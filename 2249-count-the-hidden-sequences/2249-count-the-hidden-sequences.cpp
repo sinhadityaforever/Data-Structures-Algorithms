de56b212ae5e161265664c1462240570e5748334
Aditya Sinha <sinhadityaforever@gmail.com>
2025-04-21 14:03:08 +0530

    Time: 7 ms (21.77%), Space: 112.7 MB (47.58%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long long mini = 1e9;
-        long long maxi = -1e9;
+        long long mini = 0;
+        long long maxi = 0;
         long long curr = 0;
         int n = differences.size();
-        for(int i = n-1; i>=0; i--){
-            curr-=differences[i];
+        int ans =0;
+        for(int i =0; i<n; i++){
+            curr+=differences[i];
             mini= min(curr, mini);
             maxi = max(curr, maxi);
+            if(upper-lower<maxi-mini) return 0;
         }
-        cout<<mini<<" "<<maxi<<endl;
-        int ans =0;
-        int i = upper;
-        while(i+mini >=lower){
-            if(i+maxi<=upper && i>=lower && i<=upper)
-            ans++;
-            i--;
-        }
+        return (upper-lower) - (maxi-mini)+1;
+        
         return ans;
     }
 };
